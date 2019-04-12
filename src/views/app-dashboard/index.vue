9a622821d7f0a0d5b6e1fa2b355c5ceacb004a21
hylerrix <hylerrix@gmail.com>
2019-04-12 13:27:07 +0800

    refactor：use es6 grammar to handle dynamic data

--- src/views/app-dashboard/index.vue
+++ src/views/app-dashboard/index.vue
@@ -4,7 +4,7 @@
     <!-- 卡片暂时这么处理一下，一会儿改成创享堆的卡片 -->
     <v-container fluid class="my-5">
       <v-layout row wrap>
-        <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+        <v-flex xs12 sm6 md4 lg3 v-for="person in testDeveloper" :key="person.name">
           <v-card flat class="text-xs-center ma-3">
             <v-responsive class="pt-4">
               <v-avatar size="100" class="grey lighten-2">
@@ -30,33 +30,23 @@
 
 <script lang='ts'>
   import { Component, Vue } from 'vue-property-decorator'
+  import { TestDeveloperItem } from '@/model/experiment.ts'
+  import { getTestDeveloper } from '@/api/experiment.ts'
 
   @Component({
     name: 'AppDashboard',
     components: {},
   })
   export default class AppDashboard extends Vue {
-    public team: any = [{
-        name: 'AqingCyan',
-        role: 'Web developer',
-        avatar: 'https://img2.woyaogexing.com/2019/04/11/1434ade0198c4cb89d90eafd618c8301!400x400.jpeg',
-      }, {
-        name: 'Aqing',
-        role: 'UI desinger',
-         avatar: 'https://img2.woyaogexing.com/2019/04/11/505efe51ac3c4c07b54599274ecc37ba!400x400.jpeg',
-      }, {
-        name: 'Xuekaiqi',
-        role: 'Porject Manger',
-        avatar: 'https://img2.woyaogexing.com/2019/04/11/95e5247c36af48c48d777ef393b706be!400x400.jpeg',
-      }, {
-        name: 'Cyan',
-        role: 'BC developer',
-        avatar: 'https://img2.woyaogexing.com/2019/04/11/271302eda92848828f839c4ad660ead9!400x400.jpeg',
-      }, {
-        name: 'WangTing',
-        role: 'UI desinger',
-        avatar: 'https://img2.woyaogexing.com/2019/04/11/27a7517dd10a4ae6856b7626359f9842!400x400.jpeg',
-    }]
+    public testDeveloper: TestDeveloperItem[] = []
+
+    created() {
+      this.getTestDeveloper()
+    }
+
+    async getTestDeveloper() {
+      this.testDeveloper = await getTestDeveloper()
+    }
   }
 </script>
 
